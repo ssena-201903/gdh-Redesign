2dcd0113a47b68fd674778771f96c9fd6df26070
Safiye Sena Merdin <safiyesenam@gmail.com>
2024-11-25 08:55:10 +0300

    converted .js to .jsx for icons

--- src/components/icons/AboutUsIcon.jsx
+++ src/components/icons/AboutUsIcon.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Proptypes from 'prop-types';
 
 const AboutUsIcon = ({size, color, margin}) => { 
--- src/components/icons/HeartIcon.jsx
+++ src/components/icons/HeartIcon.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PropTypes from "prop-types";
 
 const HeartIcon = ({ size, color, margin }) => {
